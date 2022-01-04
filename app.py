6858ec32931d50b058c4f0713fd0db0b5e9df0d0
Faith Muthoni <faith@Faiths-MacBook-Pro.local>
2022-01-04 18:08:32 +0300

    db model

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
 from flask import render_template
 from flask import Flask
 from flask import request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 app = Flask(__name__)
+app.config['SQLAlCHEMY_DATABASE_URI'] = 'sqlite:///friends.db'
+
+#initalize database
+db = SQLAlchemy(app)
+
+# create db model
+class Friends(db.Model):
+   id = db.Column(db.Integer,primary_key=True)
+   name = db.Column(db.String, nullable=False)
+   date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+# create a function to return a string when we add something
+   def __repr__(self):
+       return '<Name %r>' % self.id
+
 
 subscribers = []
 
@@ -28,8 +46,16 @@ def form():
    email = request.form.get('email')
    if not first_name or not last_name or not email:
       error_statement = 'All form fields are required,Kindly fill them...'
-      return render_template("fail.html", error_statement=error_statement)
-      
+      return render_template("fail.html", error_statement=error_statement,first_name = first_name,
+      last_name=last_name,email=email)
+
    subscribers.append(f"{first_name}  {last_name} | {email}")
 
    return render_template('form.html', first_name=first_name, last_name=last_name, email = email, subscribers=subscribers)
+
+@app.route('/friends', methods=['POST','GET'])
+def friends():
+   if request.method == 'POST':
+      return 'You clicked the button'
+   else:
+      return render_template ('friends.html')
